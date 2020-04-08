d3948f6fd8e9b56c053af9e0d84e44d24b638682
fatimahrizkyana <63333007+fatimahrizkyana@users.noreply.github.com>
2020-04-08 12:15:17 +0700

    menambahkan nomor hp & gmail

--- aboutme.php
+++ aboutme.php
@@ -3,4 +3,6 @@ echo 'Nama : Fatimah Rizkyana';
 echo 'Kelas : XI RPL 2';
 echo 'Asal Sekolah : SMK Taruna Bhakti Depok ';
 echo 'Alamat Rumah : Jl. Makam no 92';
+echo 'No Hp : 081617734428';
+echo 'Gmail : fatimahrizkyananuraini@gmail.com';
 ?>
